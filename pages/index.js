3f564936da7381090446d2ff9c202aa09872104d
Saxar88 <v.sasaran@protonmail.com>
2023-07-01 11:26:59 +0300

    build authentication with Google

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
+import LoginBtn from '@/components/login-btn';
+
 export default function Home() {
 	return (
-		<div className='flex items-center w-screen h-screen bg-slate-900'>
-			<div className='w-full text-center'>
-				<button className='px-4 py-2 bg-white rounded-lg'>
-					Log in with Google
-				</button>
-			</div>
+		<div className='flex justify-center w-screen h-screen bg-gray-900'>
+			<LoginBtn />
 		</div>
 	);
 }
